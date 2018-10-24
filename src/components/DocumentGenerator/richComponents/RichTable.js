75f9ef71ffd9df36e2c036fe55d9964c6983f03c
Heath Dunlop <heath.dunlop.hd@gmail.com>
2018-10-24 17:14:26 +1300

    ff

--- src/components/DocumentGenerator/richComponents/RichTable.js
+++ src/components/DocumentGenerator/richComponents/RichTable.js
@@ -11,6 +11,11 @@ import { isEmpty, isNil } from "ramda"
 
 const styles = theme => ({})
 
+const defaultTableSize = {
+  width: 550,
+  height: 300,
+}
+
 // Resizable Table outer!
 // Reuse this
 // Resize div => https://github.com/bokuweb/re-resizable
@@ -22,6 +27,30 @@ const styles = theme => ({})
 // But for now the STRML/react-resizable will fit our case. especially the handles for on hover only. i.e print. RAD MVP
 // User that third party resize component to be some sort of super. Ie resizabl;eTable. resizblae row, and cols
 class RichTable extends Component {
+  constructor(props) {
+    super(props)
+    const { classes, section, pageAttributes } = this.props
+    const { rawContent } = section
+
+    if (isNil(rawContent)) {
+      const initialTable = this._initTable(3, 3)
+      this.update(initialTable)
+      this.state = {
+        tableSize: defaultTableSize,
+      }
+    } else {
+      const { table } = rawContent
+      console.log("Peek at the table => ", table)
+      this.state = {
+        tableSize: {
+          width: table.tableSize.width,
+          height: table.tableSize.height,
+          // width: 550,
+          // height: 300,
+        },
+      }
+    }
+  }
   update = table => {
     const { classes, section } = this.props
     const { id, type, rawContent } = section
@@ -47,6 +76,44 @@ class RichTable extends Component {
       },
     }
 
+    return this.update(json)
+  }
+
+  _resizeRow = (height, rIdx) => {
+    const { section } = this.props
+
+    const updatedTableSection = section.rawContent.table
+    updatedTableSection.rows[rIdx].rowHeight = height
+
+    const json = {
+      table: {
+        ...updatedTableSection,
+      },
+    }
+
+    console.log("resize Row Table no wlooks like => ", json)
+
+    return this.update(json)
+  }
+
+  _resizeTable = (width, height) => {
+    this.setState({
+      tableSize: {
+        width: width,
+        height: height,
+      },
+    })
+    const { section } = this.props
+    const json = {
+      table: {
+        ...section.rawContent.table,
+        tableSize: {
+          width: width,
+          height: height,
+        },
+      },
+    }
+    console.log("Resized Object to update looks like ", json)
     return this.update(json)
   }
 
@@ -85,35 +152,59 @@ class RichTable extends Component {
             border: "solid 1px #ddd",
             background: "#f0f0f0",
           }}
-          defaultSize={{
-            width: 600,
-            height: 300,
-          }}>
+          onResizeStop={(e, direction, ref, d) => {
+            this._resizeTable(
+              this.state.tableSize.width + d.width,
+              this.state.tableSize.height + d.height
+            )
+          }}
+          size={this.state.tableSize}>
           <Table>
             {table.rows.map((row, rIdx) => {
               return (
-                <TableRow>
-                  {row.map((cell, cIdx) => {
-                    return (
-                      <TableCell>
-                        <RichEditor
-                          pageAttributes={pageAttributes}
-                          document={
-                            cell.document
-                              ? cell
-                              : this._generateCell(rIdx, cIdx)
-                          }
-                          // document={cell}
-                          // document={cell.document}
-                          // document={this._generateCell(rIdx, cIdx)}
-                          updateDocument={document =>
-                            this._updateCell(document, rIdx, cIdx)
-                          }
-                        />
-                      </TableCell>
+                <Resizable
+                  style={{
+                    border: "solid 1px #ddd",
+                  }}
+                  defaultSize={{ width: 150, height: row.rowHeight }}
+                  onResizeStop={(e, direction, ref, d) => {
+                    // this._resizeRow(row.rowHeight + d.height, rIdx)
+                    this._resizeRow(
+                      row.rowHeight ? row.rowHeight + d.height : 80,
+                      rIdx
                     )
-                  })}
-                </TableRow>
+                    // this._resizeTable(
+                    //   this.state.tableSize.width + d.width,
+                    //   this.state.tableSize.height + d.height
+                    // )
+                    // Resize the row
+
+                    alert("ToDo: resize row smartly")
+                  }}>
+                  I am resizable row ToDo: think carefully about this
+                </Resizable>
+                // <TableRow>
+                //   {row.map((cell, cIdx) => {
+                //     return (
+                //       <TableCell>
+                //         <RichEditor
+                //           pageAttributes={pageAttributes}
+                //           document={
+                //             cell.document
+                //               ? cell
+                //               : this._generateCell(rIdx, cIdx)
+                //           }
+                //           // document={cell}
+                //           // document={cell.document}
+                //           // document={this._generateCell(rIdx, cIdx)}
+                //           updateDocument={document =>
+                //             this._updateCell(document, rIdx, cIdx)
+                //           }
+                //         />
+                //       </TableCell>
+                //     )
+                //   })}
+                // </TableRow>
               )
             })}
           </Table>
@@ -127,6 +218,7 @@ class RichTable extends Component {
       table: {
         rows: this._initRows(numOfRows, numOfCols),
         num_of_cols: numOfCols,
+        tableSize: defaultTableSize,
       },
     }
     return json
@@ -140,7 +232,8 @@ class RichTable extends Component {
         let newCell = this._generateCell(r, c)
         cells.push(newCell)
       }
-      rows.push(cells)
+      // rows.push(cells)
+      rows.push({ cells: cells, rowHeight: 80 })
     }
     return rows
   }
